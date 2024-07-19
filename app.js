78abcb68869d8fca8eb454ee7879d465be8a7860
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 13:49:46 +0330

    feat: add /api/auth middleware

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { default: authRouter } = require("./routes/authRouter");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use("/api/auth/", authRouter);
 
 module.exports = app;
