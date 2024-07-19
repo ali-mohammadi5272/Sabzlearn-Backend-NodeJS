198ef43d2d94f14a9905463896e5944cf9858cf6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 13:04:20 +0330

    Revert "bugfix: fix urlencoded middleware's bug"
    This reverts commit 116ed168435f7a680125a953bafc934cc8065148.

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded());
 
 module.exports = app;
