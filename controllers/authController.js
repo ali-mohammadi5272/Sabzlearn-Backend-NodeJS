145302f0dc1544c89b367c87ffc5d38b4c780273
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 18:04:25 +0330

    feat: add login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const { default: userModel } = require("../models/user");
 const { default: registerValidate } = require("../validators/auth/register");
+const { default: loginValidate } = require("../validators/auth/login");
+const { generateAccessToken } = require("../utils/auth");
 
 const register = async (req, res) => {
   const isValidRequestBody = registerValidate(req.body);
@@ -27,6 +29,43 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const isValidRequestBody = loginValidate(req.body);
+  console.log("first-test-1");
+  if (!isValidRequestBody) {
+    return res.status(422).json({ message: loginValidate.error });
+  }
+  const { username, email, password } = req.body;
+  try {
+    const findedUser = await userModel
+      .findOne({
+        password,
+        $or: [{ email }, { username }],
+      })
+      .lean();
+    if (!findedUser) {
+      const existUser = await userModel.findOne({
+        $or: [{ email }, { username }],
+      });
+      if (!existUser) {
+        return res.status(404).json({ message: "User not found !!" });
+      }
+      return res
+        .status(422)
+        .json({ message: "Username/Email or Password is not valid !!" });
+    }
+    const accessToken = generateAccessToken(findedUser);
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 24,
+      httpOnly: true,
+    });
+    return res.status(200).json({ message: "Login successfully :))" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
