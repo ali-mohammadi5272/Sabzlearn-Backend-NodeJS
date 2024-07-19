8c21c67fdc64d120d9f811b2b78e8c2ca3e939b2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 20:37:11 +0330

    feat: add cookie-parser middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { default: authRouter } = require("./routes/authRouter");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth/", authRouter);
 
