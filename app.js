97ef9e1efda597c8ff7e34312010ea3fe9e0d743
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 18:05:18 +0330

    feat: add cors middleware

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { default: authRouter } = require("./routes/authRouter");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.use("/api/auth/", authRouter);
 
