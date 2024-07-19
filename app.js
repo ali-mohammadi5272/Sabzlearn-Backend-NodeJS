39e2b00ad70845bab7d8afe3f94ff4cbe6ec65fd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 09:55:43 +0330

    feat: add middlewares for request body

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(process.env.port, () => {
   console.log(`App listening on ${process.env.port}`);
 });
