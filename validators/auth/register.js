3886494096a84f7413ec14a6e7661afccd240885
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 12:44:09 +0330

    feat: create register validator

---
+++ validators/auth/register.js
@@ -0,0 +1,37 @@
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+const schema = {
+  type: "object",
+  properties: {
+    firstname: {
+      type: "string",
+      minLength: 3,
+    },
+    lastname: {
+      type: "string",
+      minLength: 3,
+    },
+    username: {
+      type: "string",
+      minLength: 3,
+    },
+    email: {
+      type: "string",
+    },
+    phone: {
+      type: "number",
+      minimum: 11,
+    },
+    password: {
+      type: "string",
+      minLength: 8,
+    },
+  },
+  required: ["firstname", "lastname", "username", "email", "phone", "password"],
+  additionalProperties: false,
+};
+
+const validate = ajv.compile(schema);
+
+module.exports.default = validate;
