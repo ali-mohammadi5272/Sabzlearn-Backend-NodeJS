3bf7b07c81cce3caaf926cf2fb2d496e6cb1f9f2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 13:48:05 +0330

    feat: register router

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { register } = require("../controllers/authController");
 
 const router = express.Router();
+
+router.post("/register", register);
 
 module.exports.default = router;
