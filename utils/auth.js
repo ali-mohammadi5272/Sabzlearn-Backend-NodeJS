1aaf8b75688e62320ee1b5d381677afa4b14ec21
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-19 08:51:01 +0330

    feat: create hashPassword function

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
+hashPassword("test-1010").then((password) => {
+  console.log(password);
+});
+
+module.exports = {
+  hashPassword,
+};
