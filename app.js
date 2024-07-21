86fbfbc8bdd38536c53d65b6221515f21b2fa2fd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-21 13:04:53 +0330

    refactor: change middleware urlEncoded

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
