aeaef5c629b4e42e36c59b24cebd079df2c79d32
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-21 10:38:50 +0330

    refactor: change port to PORT

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const { default: connectToDB } = require("./configs/db");
 const app = require("./app");
 connectToDB();
 
-app.listen(process.env.port, () => {
-  console.log(`App listening on ${process.env.port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on ${process.env.PORT}`);
 });
