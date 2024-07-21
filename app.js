5fe2c757d8330001f61f8393cdd0ef268e9d588c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-21 12:54:29 +0330

    feat: add helmet middleware

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const {
   default: authRouter,
 } = require(`./routes/${process.env.VERSION}/authRouter`);
+const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
 
