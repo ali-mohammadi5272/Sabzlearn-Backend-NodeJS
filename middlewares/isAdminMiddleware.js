79421df62528666e0d29c0220142244c4f790103
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 15:04:44 +0330

    bugfix: fix send response bug

--- middlewares/isAdminMiddleware.js
+++ middlewares/isAdminMiddleware.js
@@ -1,12 +1,12 @@
 const middleware = (req, res, next) => {
   const isAdmin = req.user.role === "ADMIN" || req.user.role === "MANAGER";
-  if (isAdmin) {
-    next();
+  if (!isAdmin) {
+    return res.status(403).json({
+      message:
+        "You are not allowed to access this page because you are not authenticated or have no permissions to access this page.",
+    });
   }
-  return res.status(403).json({
-    message:
-      "You are not allowed to access this page because you are not authenticated or have no permissions to access this page.",
-  });
+  next();
 };
 
 module.exports.default = middleware;
