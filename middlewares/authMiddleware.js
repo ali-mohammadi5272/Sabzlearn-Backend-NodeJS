940816180da53a81a10aca7bcae422f8db9d9d2d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 11:40:33 +0330

    feat: create authMiddleware to check User's token

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+const { tokenPayloadData } = require("../utils/auth");
+const { default: userModel } = require("../models/user");
+const { request } = require("http");
+
+const authMiddleware = (req, res, next) => {
+  const accessToken = req.header("Authorization").trim(" ")[1];
+  if (!accessToken) {
+    return res.status(401).json({ message: "Unauthorized !!" });
+  }
+  const tokenPayload = tokenPayloadData(accessToken);
+  if (!tokenPayload) {
+    return res.status(401).json({ message: "Unauthorized !!" });
+  }
+  const { _id } = tokenPayload;
+  const user = userModel.findOne({ _id }).select("-__v -password").lean();
+  request.user = structuredClone(user);
+  next();
+};
+
+module.exports = { authMiddleware };
