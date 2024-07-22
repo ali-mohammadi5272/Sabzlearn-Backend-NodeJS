15d6556f24f8366dae1e40974714e8262da5f0b7
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 06:40:42 +0330

    feat: add banUser function

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -3,7 +3,34 @@ const { default: userModel } = require("../../models/user");
 const { default: banUserModel } = require("../../models/banUser");
 const { phoneNumberPrefixPattern } = require("../../utils/patterns");
 
+const banUser = async (req, res) => {
+  const { phone } = req.body;
+  const changedPhoneNumber = phone.replace(phoneNumberPrefixPattern, "");
 
+  try {
+    await checkDBCollectionIndexes(banUserModel);
+  } catch (err) {
+    const isUserExistBefore = await banUserModel
+      .findOne({ phone: changedPhoneNumber })
+      .lean();
+    if (isUserExistBefore) {
+      return res.status(422).json({ message: "User is already in banList !!" });
+    }
+  }
+
+  try {
+    const newBanUser = await banUserModel.create({ phone: changedPhoneNumber });
+    if (!newBanUser) {
+      return res.status(500).json({ message: "Add User to banList failed !!" });
+    }
+    return res.status(201).json({
+      message: "User added to banList successfully :))",
+      user: newBanUser,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   banUser,
