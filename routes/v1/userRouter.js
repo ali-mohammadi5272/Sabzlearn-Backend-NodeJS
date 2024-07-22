68130638e3269e56e54705d77f70c0f2ccdb6f75
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 06:48:25 +0330

    feat: add getAll api

--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { freeUser, banUser } = require("../../controllers/v1/userController");
+const {
+  getAll,
+  freeUser,
+  banUser,
+} = require("../../controllers/v1/userController");
 
 const router = express.Router();
 
+router.get("/", getAll);
 router.post("/ban/:id", banUser);
 router.put("/free/:id", freeUser);
 
