46ce0944b72c7cfdeecfb642bee18ff1231aab2e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 21:01:55 +0330

    feat: add getCategory function

--- controllers/v1/categoryController.js
+++ controllers/v1/categoryController.js
@@ -1,3 +1,4 @@
+const { isValidObjectId } = require("mongoose");
 const { default: categoryModel } = require("../../models/category");
 const {
   checkDBCollectionIndexes,
@@ -57,7 +58,29 @@ const getAll = async (req, res) => {
   }
 };
 
+const getCategory = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+  if (!isValidId) {
+    return res.status(422).json({ message: "CategoryId is not valid !!" });
+  }
+
+  try {
+    const category = await categoryModel
+      .findOne({ _id: id })
+      .select("-__v")
+      .lean();
+    if (!category) {
+      return res.status(404).json({ message: "Categroy not found !!" });
+    }
+    return res.status(200).json(category);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   addCategroy,
   getAll,
+  getCategory,
 };
