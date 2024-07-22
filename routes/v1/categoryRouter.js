e466cb7e702ad20b9765b5d0c738d1cc78cc58f8
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 20:42:28 +0330

    feat: add getAll api

--- routes/v1/categoryRouter.js
+++ routes/v1/categoryRouter.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const { addCategroy } = require("../../controllers/v1/categoryController");
+const {
+  addCategroy,
+  getAll,
+} = require("../../controllers/v1/categoryController");
 const { default: authMiddleware } = require("../../middlewares/authMiddleware");
 const {
   default: isAdminMiddleware,
 } = require("../../middlewares/isAdminMiddleware");
 
 const router = express.Router();
-
+router.get("/", getAll);
 router.use(authMiddleware, isAdminMiddleware);
 router.route("/").post(addCategroy);
 
