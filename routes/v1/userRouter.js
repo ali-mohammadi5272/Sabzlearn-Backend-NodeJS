67494323a0f1e08304456a67db931ca914fffca1
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 13:49:34 +0330

    feat: add midlleware to apis to authorization

--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -5,12 +5,15 @@ const {
   freeUser,
   banUser,
 } = require("../../controllers/v1/userController");
-
+const { default: authMiddleware } = require("../../middlewares/authMiddleware");
+const {
+  default: isAdminMiddleware,
+} = require("../../middlewares/isAdminMiddleware");
 const router = express.Router();
 
 router.get("/", getAll);
 router.get("/:id", getUser);
-router.post("/ban/:id", banUser);
-router.put("/free/:id", freeUser);
+router.post("/ban/:id", authMiddleware, isAdminMiddleware, banUser);
+router.put("/free/:id", authMiddleware, isAdminMiddleware, freeUser);
 
 module.exports.default = router;
