7f84068c801b0ba778fcd34d647c2bdd734e3647
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 20:35:49 +0330

    feat: add authorization middlewares to router

--- routes/v1/categoryRouter.js
+++ routes/v1/categoryRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { addCategroy } = require("../../controllers/v1/categoryController");
+const { default: authMiddleware } = require("../../middlewares/authMiddleware");
+const {
+  default: isAdminMiddleware,
+} = require("../../middlewares/isAdminMiddleware");
 
 const router = express.Router();
 
+router.use(authMiddleware, isAdminMiddleware);
 router.route("/").post(addCategroy);
 
 module.exports.default = router;
