c42db3d22c04bd8b374697dd45d08508ebff369d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 07:26:48 +0330

    docs: remove banUserController and banUser and freeUser fuction from authRouter

--- routes/v1/authRouter.js
+++ routes/v1/authRouter.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const { register, login } = require("../../controllers/v1/authController");
-const { banUser, freeUser } = require("../../controllers/v1/banUserController");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/banUser", banUser);
-router.delete("/freeUser/:id", freeUser);
 
 module.exports.default = router;
