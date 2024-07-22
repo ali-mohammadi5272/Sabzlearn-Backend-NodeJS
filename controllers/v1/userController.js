14c8ce3d63badab44e4d056f72235ee1d39bb111
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 06:41:19 +0330

    feat: add freeUser function

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -32,6 +32,32 @@ const banUser = async (req, res) => {
   }
 };
 
+const freeUser = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+  if (!isValidId) {
+    return res.status(422).json({ message: "UserId is not valid !!" });
+  }
+  try {
+    const existUser = await banUserModel.findOne({ _id: id }).lean();
+    if (!existUser) {
+      return res.status(404).json({ message: "User not found !!" });
+    }
+    const deletedUser = await banUserModel.findOneAndDelete({ _id: id });
+    if (!deletedUser) {
+      return res
+        .status(500)
+        .json({ message: "Remove User from banList failed !!" });
+    }
+    return res
+      .status(200)
+      .json({ message: "User removed from banList successfully :))" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   banUser,
+  freeUser,
 };
