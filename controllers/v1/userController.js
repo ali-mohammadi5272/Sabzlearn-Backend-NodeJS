513d4bd0c42573cbfe5815293236593cef19ed76
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-22 18:35:09 +0330

    feat: create updateUser function

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -3,8 +3,13 @@ const { default: userModel } = require("../../models/user");
 const { default: banUserModel } = require("../../models/banUser");
 const { roles } = require("../../utils/constants");
 const {
+  default: updateUserValidate,
+} = require("../../validators/auth/updateUser");
+const {
   checkDBCollectionIndexes,
 } = require("../../utils/checkCollectionIndexes");
+const { hashPassword, generateAccessToken } = require("../../utils/auth");
+const { phoneNumberPrefixPattern } = require("../../utils/patterns");
 
 const getAll = async (req, res) => {
   try {
@@ -91,6 +96,46 @@ const changeRole = async (req, res) => {
   }
 };
 
+const updateUser = async (req, res) => {
+  const isValidRequestBody = updateUserValidate(req.body);
+  if (!isValidRequestBody) {
+    return res.status(422).json(updateUserValidate.errors);
+  }
+
+  const { password, phone } = req.body;
+  const changedPhoneNumber = phone.replace(phoneNumberPrefixPattern, "");
+
+  try {
+    const hashedPassword = await hashPassword(password);
+    const updatedUser = await userModel
+      .findByIdAndUpdate(
+        { _id: req.user._id },
+        {
+          ...req.body,
+          phone: changedPhoneNumber,
+          password: hashedPassword,
+        }
+      )
+      .select("-__v -password");
+    if (!updatedUser) {
+      return res.status(500).json({ message: "User update faild !!" });
+    }
+
+    const accessToken = generateAccessToken({ _id: updatedUser._id });
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 24,
+      httpOnly: true,
+    });
+    return res.status(201).json({
+      message: "User updated successfully :))",
+      user: updatedUser,
+      accessToken,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 const banUser = async (req, res) => {
   const { id } = req.params;
   const isValidId = isValidObjectId(id);
@@ -168,6 +213,7 @@ module.exports = {
   getUser,
   removeUser,
   changeRole,
+  updateUser,
   banUser,
   freeUser,
 };
