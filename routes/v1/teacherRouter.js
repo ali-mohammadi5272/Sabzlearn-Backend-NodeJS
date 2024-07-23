d95a1d7fb7aca9eaa850d47cb629da273d009d9e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-23 13:31:49 +0330

    feat: create removeTeacher api

--- routes/v1/teacherRouter.js
+++ routes/v1/teacherRouter.js
@@ -3,12 +3,16 @@ const { default: authMiddleware } = require("../../middlewares/authMiddleware");
 const {
   default: isAdminMiddleware,
 } = require("../../middlewares/isAdminMiddleware");
-const { addTeacher } = require("../../controllers/v1/teacherController");
+const {
+  addTeacher,
+  removeTeacher,
+} = require("../../controllers/v1/teacherController");
 
 const router = express.Router();
 
 router.use(authMiddleware, isAdminMiddleware);
 
 router.route("/").post(addTeacher);
+router.route("/:id").delete(removeTeacher);
 
 module.exports.default = router;
