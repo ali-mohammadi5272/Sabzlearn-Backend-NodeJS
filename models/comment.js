34599d17f0373ebd0b4fd815fcd0a9b84a8f1ba3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-23 12:26:50 +0330

    feat: create comment model

---
+++ models/comment.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    body: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    score: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+      default: 5,
+    },
+    isAccepted: {
+      type: Boolean,
+      enum: [true, false],
+      default: false,
+    },
+    isAnswer: {
+      type: Boolean,
+      enum: [true, false],
+      required: true,
+    },
+    mainCommentId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Comment",
+    },
+  },
+  { timestamps: true }
+);
