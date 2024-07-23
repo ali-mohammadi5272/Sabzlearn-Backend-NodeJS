a18a10227a315e140e37c843bd9f1593881aa6af
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-23 09:59:00 +0330

    feat: add sessionPattern RegEx

--- utils/patterns.js
+++ utils/patterns.js
@@ -1,4 +1,9 @@
 const phoneNumberPattern = /^(0|\+98)?9\d{9}$/;
 const phoneNumberPrefixPattern = /^(0|\+98)/;
+const sessionTimePattern = /^[0-2][0-3]:[0-5][0-9]$/;
 
-module.exports = { phoneNumberPattern, phoneNumberPrefixPattern };
+module.exports = {
+  phoneNumberPattern,
+  phoneNumberPrefixPattern,
+  sessionTimePattern,
+};
