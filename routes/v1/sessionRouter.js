7276db74ab1977883d52c90e9c3906478a7a0de4
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-25 19:17:23 +0330

    feat: create Session Router

---
+++ routes/v1/sessionRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { addSession } = require("../../controllers/v1/sessionController");
+const { uploader } = require("../../utils/uploader");
+const { roles } = require("../../utils/constants");
+const { default: authMiddleware } = require("../../middlewares/authMiddleware");
+const {
+  default: accessLevelMiddleware,
+} = require("../../middlewares/accessLevelMiddleware");
+
+const router = express.Router();
+
+router.use(authMiddleware, accessLevelMiddleware(roles.teacher));
+router
+  .route("/")
+  .post(uploader("public/courses/sessions").single("upload"), addSession);
+
+module.exports.default = router;
