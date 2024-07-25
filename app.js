db5f9f67483dca48a3c30d99d9d657d872c3328a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-25 17:35:15 +0330

    feat: add static path

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(process.cwd(), "public")));
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
