dc639ee1b5653e1769b3b5e7d735f3dd29a7a16e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-25 19:18:04 +0330

    refactor: change uploader and make it better

--- utils/uploader.js
+++ utils/uploader.js
@@ -1,22 +1,23 @@
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(process.cwd(), "./public/courses/covers"));
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix =
-      Date.now() +
-      "-" +
-      crypto.randomUUID() +
-      Math.round(Math.random() * 1e9 * 987);
-    const fileExtension = path.extname(file.originalname);
-    const fileFullName = `${uniqueSuffix}${fileExtension}`;
-    cb(null, fileFullName);
-  },
-});
-
-const uploader = multer({ storage: storage });
+const uploader = (pathName) => {
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(process.cwd(), pathName));
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix =
+        Date.now() +
+        "-" +
+        crypto.randomUUID() +
+        Math.round(Math.random() * 1e9 * 987);
+      const fileExtension = path.extname(file.originalname);
+      const fileFullName = `${uniqueSuffix}${fileExtension}`;
+      cb(null, fileFullName);
+    },
+  });
+  return multer({ storage });
+};
 
 module.exports = { uploader };
