cddf57ef536a2a718de5e84fbd949e7c4e2dbc2e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-25 17:19:29 +0330

    feat: create addCourse function

---
+++ controllers/v1/courseController.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const { default: courseModel } = require("../../models/course");
+const {
+  checkDBCollectionIndexes,
+} = require("../../utils/checkCollectionIndexes");
+const { default: courseValidate } = require("../../validators/courses/course");
+
+const addCourse = async (req, res) => {
+  req.body.price = +req.body.price;
+  req.body.qualification = JSON.parse(req.body.qualification);
+  req.body.discount = +req.body.discount;
+
+  const isValidRequestBody = courseValidate(req.body);
+  if (!isValidRequestBody) {
+    return res.status(422).json(courseValidate.errors);
+  }
+
+  const { title } = req.body;
+
+  try {
+    await checkDBCollectionIndexes(courseModel);
+  } catch (err) {
+    const isUserExistBefore = await courseModel
+      .findOne({
+        $or: [{ title }],
+      })
+      .lean();
+    if (isUserExistBefore) {
+      return res.status(422).json({ message: "Course is already exist !!" });
+    }
+  }
+
+  try {
+    const newCourse = await courseModel.create({
+      ...req.body,
+      teacherId: req.user._id,
+      cover: req.file.filename,
+    });
+    if (!newCourse) {
+      return res.status(500).json({ message: "Add Course failed !!" });
+    }
+
+    const findedCourse = await courseModel
+      .findOne({ _id: newCourse._id })
+      .populate("teacherId categoryId", "firstname lastname title href")
+      .lean();
+
+    return res.status(201).json({
+      message: "Course added successfully :))",
+      category: findedCourse,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { addCourse };
