5eb0fd707198919588946339f2322e2aab3a7b14
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-28 18:21:32 +0330

    feat: create getSession function

--- controllers/v1/sessionController.js
+++ controllers/v1/sessionController.js
@@ -7,6 +7,7 @@ const {
   default: sessionValidate,
 } = require("../../validators/sessions/session");
 
+
 const getAll = async (req, res) => {
   try {
     const sessions = await sessionModel
@@ -68,4 +69,25 @@ const addSession = async (req, res) => {
   }
 };
 
-module.exports = { getAll, addSession };
+const getSession = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+  if (!isValidId) {
+    return res.status(422).json({ message: "SessionId is not valid !!" });
+  }
+
+  try {
+    const session = await sessionModel
+      .findOne({ _id: id })
+      .select("-__v")
+      .lean();
+    if (!session) {
+      return res.status(404).json({ message: "Session not found !!" });
+    }
+    return res.status(200).json(session);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAll, addSession, getSession };
