9ac5e9b30f3db32d870c9ae4efea41273667429a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-31 11:55:44 +0330

    feat: add addComment api

--- routes/v1/commentRouter.js
+++ routes/v1/commentRouter.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { addComment } = require("../../controllers/v1/commentController");
+const { default: authMiddleware } = require("../../middlewares/authMiddleware");
 
 const router = express.Router();
+
+router.use(authMiddleware);
+router.route("/").post(addComment);
 
 module.exports.default = router;
