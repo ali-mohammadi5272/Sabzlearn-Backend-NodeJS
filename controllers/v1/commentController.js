9a51aa5e2f6714513f01cfff137f8190a0f27183
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-31 11:55:04 +0330

    feat: create addComment function

---
+++ controllers/v1/commentController.js
@@ -0,0 +1,40 @@
+const { default: commentModel } = require("../../models/comment");
+const { default: courseModel } = require("../../models/course");
+
+const {
+  default: commentValidate,
+} = require("../../validators/comments/comment");
+
+const addComment = async (req, res) => {
+  const isValidRequestBody = commentValidate(req.body);
+  if (!isValidRequestBody) {
+    return res.status(422).json(commentValidate.errors);
+  }
+
+  try {
+    const course = courseModel.findOne({ _id: req.body.courseId }).lean();
+    if (!course) {
+      return res.status(422).json({ message: "Course not found !!" });
+    }
+    const newComment = await commentModel.create({
+      ...req.body,
+      userId: req.user._id,
+      isAccepted: false,
+    });
+    if (!newComment) {
+      return res.status(500).json({ message: "Add Comment failed !!" });
+    }
+
+    const newCommentObject = newComment.toObject();
+    Reflect.deleteProperty(newCommentObject, "__v");
+
+    return res.status(201).json({
+      message: "Comment added successfully :))",
+      comment: newCommentObject,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { addComment };
