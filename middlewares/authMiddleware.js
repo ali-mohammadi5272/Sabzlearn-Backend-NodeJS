2307a9cccb341de55c5e29b24f606cf5a71faba5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-03 17:02:48 +0330

    Revert "refactor: change isUserRegisterInApplication to userRegisterInApplicationInfo"
    This reverts commit b38e52c4664b172d5ea226eb23e73bc843cb6660.

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const { isUserRegisterInApplication } = require("../utils/auth");
 
 const middleware = async (req, res, next) => {
   try {
-    const user = await userRegisterInApplicationInfo(req);
+    const user = await isUserRegisterInApplication(req);
     if (!user) {
       return res.status(401).json({ message: "Unauthorized !!" });
     }
