4765bb5218b6b1a2261b780103eda1022b6558c5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-03 20:34:31 +0330

    feat: create acceptComment function

--- controllers/v1/commentController.js
+++ controllers/v1/commentController.js
@@ -63,4 +63,32 @@ const removeComment = async (req, res) => {
   }
 };
 
-module.exports = { addComment, removeComment };
+const acceptComment = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+  if (!isValidId) {
+    return res.status(422).json({ message: "CommentId is not valid !!" });
+  }
+
+  try {
+    const comment = await commentModel
+      .findByIdAndUpdate(
+        { _id: id },
+        {
+          isAccepted: true,
+        }
+      )
+      .populate("userId courseId", "username title cover")
+      .select("-__v");
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found !!" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Comment updated successfully :))", comment });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { addComment, removeComment, acceptComment };
