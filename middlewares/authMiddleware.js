f1b19faeae469ac51e7d2be86b5a57b11b229869
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-03 16:56:43 +0330

    refactor: make middleware better

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,12 @@
-const { tokenPayloadData } = require("../utils/auth");
-const { default: userModel } = require("../models/user");
+const { isUserRegisterInApplication } = require("../utils/auth");
 
 const middleware = async (req, res, next) => {
-  const accessToken = req.header("Authorization")?.split(" ")[1];
-  if (!accessToken) {
-    return res.status(401).json({ message: "Unauthorized !!" });
-  }
-  const tokenPayload = tokenPayloadData(accessToken);
-  if (!tokenPayload) {
-    return res.status(401).json({ message: "Unauthorized !!" });
-  }
-  const { _id } = tokenPayload;
   try {
-    const user = await userModel
-      .findOne({ _id })
-      .select("-__v -password")
-      .lean();
-    req.user = Object.assign({}, user);
+    const user = await isUserRegisterInApplication(req);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized !!" });
+    }
+    req.user = user;
     next();
   } catch (error) {
     return res.status(500).json({ message: error.message });
