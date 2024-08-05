0d75d436571a6b8a7b7e0dc5af58f582c1672cfc
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 17:32:17 +0330

    feat: create answerMessage api

--- routes/v1/contactUsRouter.js
+++ routes/v1/contactUsRouter.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { sendMessage } = require("../../controllers/v1/contactUsController");
+const authMiddleware = require("../../middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
+const { roles } = require("../../utils/constants");
+const {
+  sendMessage,
+  answerMessage,
+} = require("../../controllers/v1/contactUsController");
 
 const router = express.Router();
 
 router.route("/").post(sendMessage);
+router.use(authMiddleware, accessLevelMiddleware(roles.admin));
+router.route("/answer/:id").post(answerMessage);
 
 module.exports = router;
