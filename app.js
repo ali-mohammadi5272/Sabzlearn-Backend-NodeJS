10e8bae7fa1ce933661d2811091c663ed28266bd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 11:42:18 +0330

    feat: create ContactUs api router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const categoryRouter = require(`./routes/${VERSION}/categoryRouter`);
 const courseRouter = require(`./routes/${VERSION}/courseRouter`);
 const sessionRouter = require(`./routes/${VERSION}/sessionRouter`);
 const commentRouter = require(`./routes/${VERSION}/commentRouter`);
+const contactUsRouter = require(`./routes/${VERSION}/contactUsRouter`);
 
 const helmet = require("helmet");
 const cors = require("cors");
@@ -27,6 +28,7 @@ app.use(`/api/${VERSION}/categories/`, categoryRouter);
 app.use(`/api/${VERSION}/courses/`, courseRouter);
 app.use(`/api/${VERSION}/sessions/`, sessionRouter);
 app.use(`/api/${VERSION}/comments/`, commentRouter);
+app.use(`/api/${VERSION}/contactUs/`, contactUsRouter);
 
 app.use((err, req, res, next) => {
   return res.status(500).json({ message: err.message });
