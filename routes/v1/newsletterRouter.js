c8f356e9632c8c0ef95d766eb84ba7d61d4bf238
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 19:25:00 +0330

    feat: create getAll api

--- routes/v1/newsletterRouter.js
+++ routes/v1/newsletterRouter.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const authMiddleware = require("../../middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
+const { roles } = require("../../utils/constants");
+const { getAll } = require("../../controllers/v1/newsletterController");
 
 const router = express.Router();
+
+router.use(authMiddleware, accessLevelMiddleware(roles.admin));
+
+router.route("/").get(getAll);
 
 module.exports = router;
