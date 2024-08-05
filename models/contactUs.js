beb1fc8b9feca21b2f419398c8825ce916a5d714
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 18:33:07 +0330

    feat: add timestamps to model

--- models/contactUs.js
+++ models/contactUs.js
@@ -1,37 +1,40 @@
 const mongoose = require("mongoose");
 const { emailPattern, phoneNumberPattern } = require("../utils/patterns");
 
-const schema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: true,
-    minLength: 5,
-    trim: true,
+const schema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      minLength: 5,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: emailPattern,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      match: phoneNumberPattern,
+      trim: true,
+    },
+    body: {
+      type: String,
+      required: true,
+      minLength: 1,
+      trim: true,
+    },
+    hasBeenAnswered: {
+      type: Boolean,
+      default: false,
+      trim: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    match: emailPattern,
-    trim: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    match: phoneNumberPattern,
-    trim: true,
-  },
-  body: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true,
-  },
-  hasBeenAnswered: {
-    type: Boolean,
-    default: false,
-    trim: true,
-  },
-});
+  { timestamps: true }
+);
 
 const model = mongoose.models.ContactUs || mongoose.model("ContactUs", schema);
 
