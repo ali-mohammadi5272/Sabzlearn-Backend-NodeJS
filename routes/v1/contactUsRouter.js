5525cc04f31b943b051440abf6bc5e68ce631ce0
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 16:00:49 +0330

    feat: create sendMessage api

--- routes/v1/contactUsRouter.js
+++ routes/v1/contactUsRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { sendMessage } = require("../../controllers/v1/contactUsController");
 
 const router = express.Router();
+
+router.route("/").post(sendMessage);
 
 module.exports = router;
