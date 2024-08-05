b9d896430e02ad7d3d4aa1aad6efa803d9faabe6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 19:48:53 +0330

    feat: create registerNewsletter function

--- controllers/v1/newsletterController.js
+++ controllers/v1/newsletterController.js
@@ -1,4 +1,8 @@
 const newsletterModel = require("../../models/newsletter");
+const newsletterValidate = require("../../validators/newsletter/newsletter");
+const {
+  checkDBCollectionIndexes,
+} = require("../../utils/checkCollectionIndexes");
 
 const getAll = async (req, res) => {
   try {
@@ -12,4 +16,41 @@ const getAll = async (req, res) => {
   }
 };
 
-module.exports = { getAll };
+const registerNewsletter = async (req, res) => {
+  const isValidRequestBody = newsletterValidate(req.body);
+  if (!isValidRequestBody) {
+    return res.status(422).json(newsletterValidate.errors);
+  }
+
+  const { email } = req.body;
+
+  try {
+    await checkDBCollectionIndexes(newsletterModel);
+  } catch (err) {
+    const isEmailExistBefore = await newsletterModel.findOne({
+      $or: [{ email }],
+    });
+    if (isEmailExistBefore) {
+      return res.status(422).json({ message: "Email is already exist !!" });
+    }
+  }
+
+  try {
+    const newletter = await newsletterModel.create(req.body);
+    if (!newletter) {
+      return res.status(500).json({ message: "Add Newsletter failed !!" });
+    }
+
+    const newletterObject = newletter.toObject();
+    Reflect.deleteProperty(newletterObject, "__v");
+
+    return res.status(201).json({
+      message: "Newsletter added successfully :))",
+      newletter: newletterObject,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAll, registerNewsletter };
