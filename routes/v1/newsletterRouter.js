80c0b5c5556b5e73108068386a45b7b84a55775a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-05 19:47:40 +0330

    feat: create registerNewsletter api

--- routes/v1/newsletterRouter.js
+++ routes/v1/newsletterRouter.js
@@ -2,9 +2,14 @@ const express = require("express");
 const authMiddleware = require("../../middlewares/authMiddleware");
 const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
 const { roles } = require("../../utils/constants");
-const { getAll } = require("../../controllers/v1/newsletterController");
+const {
+  getAll,
+  registerNewsletter,
+} = require("../../controllers/v1/newsletterController");
 
 const router = express.Router();
+
+router.route("/register").post(registerNewsletter);
 
 router.use(authMiddleware, accessLevelMiddleware(roles.admin));
 
