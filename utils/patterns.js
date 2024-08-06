e08a31335dfad527a2aca72aad693dd424e1cd65
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-06 17:46:58 +0330

    bugfix: fix email regex pattern bug

--- utils/patterns.js
+++ utils/patterns.js
@@ -1,4 +1,4 @@
-const emailPattern = /^[\w\.-_]+@[\w]{5,8}\.[a-z]{2,3}$/;
+const emailPattern = /^[\w\.\-_]+@[\w]{5,8}\.[a-z]{2,3}$/;
 const phoneNumberPattern = /^(0|\+98)?9\d{9}$/;
 const phoneNumberPrefixPattern = /^(0|\+98)/;
 const sessionTimePattern = /^[0-5][0-9]:(([1-5][0-9])|([0-5][1-9]))$/;
