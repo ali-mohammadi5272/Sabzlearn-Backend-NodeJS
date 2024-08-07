b77605c94db04f8854090b260831df36b9594f8a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-07 10:32:01 +0330

    feat: create discountAll api

--- routes/v1/discountCodeRouter.js
+++ routes/v1/discountCodeRouter.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const authMiddleware = require("../../middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
+const { roles } = require("../../utils/constants");
+const {
+  discountAllCourses,
+} = require("../../controllers/v1/discountCodeController");
 
 const router = express.Router();
+
+router
+  .route("/all")
+  .put(
+    authMiddleware,
+    accessLevelMiddleware(roles.manager),
+    discountAllCourses
+  );
 
 module.exports = router;
