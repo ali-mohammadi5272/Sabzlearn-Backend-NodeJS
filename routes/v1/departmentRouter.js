7aecf10fbff7bbfcc6a38221f6f5ca2c9903092a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-13 14:42:05 +0330

    feat: only admin and manager can add departments

--- routes/v1/departmentRouter.js
+++ routes/v1/departmentRouter.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const authMiddleware = require("../../middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
 const {
   addDepartment,
   getAll,
 } = require("../../controllers/v1/departmentController");
+const { roles } = require("../../utils/constants");
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.route("/").get(getAll).post(addDepartment);
+router
+  .route("/")
+  .get(getAll)
+  .post(accessLevelMiddleware(roles.admin), addDepartment);
 
 module.exports = router;
