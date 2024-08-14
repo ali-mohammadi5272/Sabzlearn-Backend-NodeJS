8decbad3864cd291feb410375494e5f78823e266
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-14 13:49:05 +0330

    feat: create answerTicketByAdmin api

--- routes/v1/ticketRouter.js
+++ routes/v1/ticketRouter.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const authMiddleware = require("../../middlewares/authMiddleware");
 const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
-const { createTicket } = require("../../controllers/v1/ticketController");
+const {
+  createTicket,
+  answerTicketByAdmin,
+} = require("../../controllers/v1/ticketController");
 const { roles } = require("../../utils/constants");
 
 const router = express.Router();
 
 router.route("/").post(authMiddleware, createTicket);
+router
+  .route("/answer/:id")
+  .post(
+    authMiddleware,
+    accessLevelMiddleware(roles.admin),
+    answerTicketByAdmin
+  );
 
 module.exports = router;
