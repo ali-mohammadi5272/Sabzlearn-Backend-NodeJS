63ac1c70d6f068a269a61fccb9644da8ed9bf31b
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-14 13:48:45 +0330

    feat: create answerTicketByAdmin function

--- controllers/v1/ticketController.js
+++ controllers/v1/ticketController.js
@@ -1,6 +1,8 @@
 const ticketModel = require("../../models/ticket");
 const departmentModel = require("../../models/department");
 const sendTicketValidate = require("../../validators/tickets/sendTicket");
+const answerTicketValidate = require("../../validators/tickets/answerTicket");
+const { isValidObjectId } = require("mongoose");
 
 const createTicket = async (req, res) => {
   const isValidRequestBody = sendTicketValidate(req.body);
@@ -41,6 +43,58 @@ const createTicket = async (req, res) => {
   }
 };
 
+const answerTicketByAdmin = async (req, res) => {
+  const isValidRequestBody = answerTicketValidate(req.body);
+  if (!isValidRequestBody) {
+    return res.status(422).json(answerTicketValidate.errors);
+  }
+
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+  if (!isValidId) {
+    return res.status(422).json({ message: "TicketId is not valid !!" });
+  }
+
+  const { body } = req.body;
+
+  try {
+    const ticket = await ticketModel.findOneAndUpdate(
+      { _id: id },
+      {
+        hasBeenAnswered: 1,
+      }
+    );
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found !!" });
+    }
+
+    const answerTicket = await ticketModel.create({
+      title: "Ticket's Answer",
+      body,
+      userId: req.user._id,
+      departmentId: ticket.departmentId,
+      hasBeenAnswered: 0,
+      isAnswer: 1,
+      mainTicketId: ticket._id,
+    });
+
+    if (!answerTicket) {
+      return res.status(404).json({ message: "Add Answer Ticket faild !!" });
+    }
+
+    const answerTicketObject = answerTicket.toObject();
+    Reflect.deleteProperty(answerTicketObject, "__v");
+
+    return res.status(200).json({
+      message: "Answer Ticket added successfully :))",
+      ticket: answerTicketObject,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createTicket,
+  answerTicketByAdmin,
 };
