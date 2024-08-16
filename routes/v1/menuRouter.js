28106c93c04dfd4302d6c1feb2d8f3188757566c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-16 19:24:57 +0330

    feat: create getAll api

--- routes/v1/menuRouter.js
+++ routes/v1/menuRouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const authMiddleware = require("../../middlewares/authMiddleware");
 const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
 const { roles } = require("../../utils/constants");
-const { addMenu } = require("../../controllers/v1/menuController");
+const { addMenu, getAll } = require("../../controllers/v1/menuController");
 
 const router = express.Router();
 
 router
   .route("/")
+  .get(getAll)
   .post(authMiddleware, accessLevelMiddleware(roles.admin), addMenu);
 
 module.exports = router;
