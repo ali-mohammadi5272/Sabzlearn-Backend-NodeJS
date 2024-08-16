6b705737edf780ce40a1f8f1abd8facc1b75c03a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-16 19:00:04 +0330

    feat: create addMenu function

---
+++ controllers/v1/menuController.js
@@ -0,0 +1,49 @@
+const menuModel = require("../../models/menu");
+const addMenuValidate = require("../../validators/menus/addMenu");
+const { isValidObjectId } = require("mongoose");
+const {
+  checkDBCollectionIndexes,
+} = require("../../utils/checkCollectionIndexes");
+
+const addMenu = async (req, res) => {
+  const isValidRequestBody = addMenuValidate(req.body);
+  if (!isValidRequestBody) {
+    return res.status(422).json(addMenuValidate.errors);
+  }
+
+  const { title, href } = req.body;
+
+  try {
+    await checkDBCollectionIndexes(menuModel);
+  } catch (err) {
+    const isMenuExistBefore = await menuModel
+      .findOne({
+        $or: [{ title }, { href }],
+      })
+      .lean();
+    if (isMenuExistBefore) {
+      return res.status(422).json({ message: "Category is already exist !!" });
+    }
+  }
+
+  try {
+    const newMenu = await menuModel.create(req.body);
+    if (!newMenu) {
+      return res.status(500).json({ message: "Add Menu failed !!" });
+    }
+
+    const newMenuObject = newMenu.toObject();
+    Reflect.deleteProperty(newMenuObject, "__v");
+
+    return res.status(201).json({
+      message: "Menu added successfully :))",
+      menu: newMenuObject,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  addMenu,
+};
