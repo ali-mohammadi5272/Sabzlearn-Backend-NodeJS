9b2ff95bd7c9f7ac39f647f5ad394265f17e0a22
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-16 18:58:08 +0330

    docs: create menu model

---
+++ models/menu.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    href: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    parent: {
+      type: mongoose.Types.ObjectId,
+      ref: "Menu",
+      trim: true,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+schema.virtual("subMenu", {
+  ref: "Menu",
+  localField: "_id",
+  foreignField: "parent",
+});
+
+const model = mongoose.models.Menu || mongoose.model("Menu", schema);
+
+module.exports = model;
