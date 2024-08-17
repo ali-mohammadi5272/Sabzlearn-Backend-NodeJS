af46eca9beea5836488ccd6c0ae0daf34929e994
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-17 16:12:18 +0330

    feat: now cors middleware is at top level

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(process.cwd(), "public")));
 app.use(helmet());
-app.use(cors());
 app.use(cookieParser());
 
 app.use(`/api/${VERSION}/auth/`, authRouter);
