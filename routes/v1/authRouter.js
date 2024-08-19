292792707364441c6dd9f6b765d170125850bb3a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-19 10:53:29 +0330

    feat: create logout api

--- routes/v1/authRouter.js
+++ routes/v1/authRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { register, login } = require("../../controllers/v1/authController");
+const {
+  register,
+  login,
+  logout,
+} = require("../../controllers/v1/authController");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
