16c2969979c3155af46d7ed3d540c06b4603d9fd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-20 17:30:00 +0330

    docs: create basic files

---
+++ modules/v1/category/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const authMiddleware = require("../../middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
+const { roles } = require("../../utils/constants");
+const {
+  addCategroy,
+  getAll,
+  getCategory,
+  removeCategory,
+  updateCategory,
+} = require("../../controllers/v1/categoryController");
+
+const router = express.Router();
+
+router.get("/", getAll);
+router.get("/:id", getCategory);
+router.use(authMiddleware, accessLevelMiddleware(roles.admin));
+router.route("/").post(addCategroy);
+router.route("/:id").delete(removeCategory).put(updateCategory);
+
+module.exports = router;
