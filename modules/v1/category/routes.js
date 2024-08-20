2ba2b8e02d0f29a7ef0dcd1ea4b3436cdf50ed73
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-20 17:34:59 +0330

    refactor: change category router require address

--- modules/v1/category/routes.js
+++ modules/v1/category/routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const authMiddleware = require("../../middlewares/authMiddleware");
-const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
-const { roles } = require("../../utils/constants");
+const authMiddleware = require("../../../utils/middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../../utils/middlewares/accessLevelMiddleware");
+const { roles } = require("../../../utils/constants");
 const {
   addCategroy,
   getAll,
   getCategory,
   removeCategory,
   updateCategory,
-} = require("../../controllers/v1/categoryController");
+} = require("./controller");
 
 const router = express.Router();
 
