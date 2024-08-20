5515d312fee53af7ff8fdb53844f696a32e539be
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-20 19:58:30 +0330

    refactor: change notification router require address

--- modules/v1/notification/routes.js
+++ modules/v1/notification/routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const authMiddleware = require("../../middlewares/authMiddleware");
-const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
-const { roles } = require("../../utils/constants");
+const authMiddleware = require("../../../utils/middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../../utils/middlewares/accessLevelMiddleware");
+const { roles } = require("../../../utils/constants");
 const {
   sendNotification,
   getNotificationsByAdmin,
   seeNotification,
   removeNotification,
   getAdminNotificationsByManager,
-} = require("../../controllers/v1/notificationController");
+} = require("./controller");
 
 const router = express.Router();
 
