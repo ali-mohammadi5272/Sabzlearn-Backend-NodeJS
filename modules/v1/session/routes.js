66ed4a304caf5fde318a5d76727fc9e267e98828
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-20 20:12:30 +0330

    docs: create basic files

---
+++ modules/v1/session/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { uploader } = require("../../utils/uploader");
+const { roles } = require("../../utils/constants");
+const authMiddleware = require("../../middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../middlewares/accessLevelMiddleware");
+const {
+  getAll,
+  addSession,
+  getSession,
+  getSessionAndAllCourseSessions,
+  removeSession,
+} = require("../../controllers/v1/sessionController");
+
+const router = express.Router();
+
+router.route("/:courseId/:sessionId").get(getSessionAndAllCourseSessions);
+router.route("/:id").get(getSession);
+
+router.use(authMiddleware, accessLevelMiddleware(roles.teacher));
+router
+  .route("/")
+  .get(getAll)
+  .post(uploader("public/courses/sessions").single("upload"), addSession);
+router.route("/:id").delete(removeSession);
+
+module.exports = router;
