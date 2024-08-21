6f54065773a4e33c5ef99cea3b16bb6eb90b7085
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-21 08:26:16 +0330

    feat: create getAllByManager router

--- modules/v1/contactUs/routes.js
+++ modules/v1/contactUs/routes.js
@@ -2,15 +2,13 @@ const express = require("express");
 const authMiddleware = require("../../../utils/middlewares/authMiddleware");
 const accessLevelMiddleware = require("../../../utils/middlewares/accessLevelMiddleware");
 const { roles } = require("../../../utils/constants");
-const {
-  sendMessage,
-  answerMessage,
-} = require("./controller");
+const { sendMessage, answerMessage, getAllByManager } = require("./controller");
 
 const router = express.Router();
 
 router.route("/").post(sendMessage);
 router.use(authMiddleware, accessLevelMiddleware(roles.admin));
 router.route("/answer/:id").post(answerMessage);
+router.route("/all").get(getAllByManager);
 
 module.exports = router;
