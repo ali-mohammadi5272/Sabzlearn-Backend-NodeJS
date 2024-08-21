c60c90b5dd0180f552ec266eaba1a668c15fa8e2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-21 08:27:07 +0330

    feat: create getAllUnAnswered router

--- modules/v1/contactUs/routes.js
+++ modules/v1/contactUs/routes.js
@@ -2,7 +2,12 @@ const express = require("express");
 const authMiddleware = require("../../../utils/middlewares/authMiddleware");
 const accessLevelMiddleware = require("../../../utils/middlewares/accessLevelMiddleware");
 const { roles } = require("../../../utils/constants");
-const { sendMessage, answerMessage, getAllByManager } = require("./controller");
+const {
+  sendMessage,
+  answerMessage,
+  getAllByManager,
+  getAllUnAnswered,
+} = require("./controller");
 
 const router = express.Router();
 
@@ -10,5 +15,6 @@ router.route("/").post(sendMessage);
 router.use(authMiddleware, accessLevelMiddleware(roles.admin));
 router.route("/answer/:id").post(answerMessage);
 router.route("/all").get(getAllByManager);
+router.route("/").get(getAllUnAnswered);
 
 module.exports = router;
