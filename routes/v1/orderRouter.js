6e363d4c8649baa7ddbb4074d5a5070ee3a961b5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-21 08:16:28 +0330

    docs: delete useless old Structure's files

--- routes/v1/orderRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authMiddleware = require("../../middlewares/authMiddleware");
-const { getUserOrders } = require("../../controllers/v1/userOrderController");
-
-const router = express.Router();
-
-router.route("/user").get(authMiddleware, getUserOrders);
-
-module.exports = router;
--- routes/v1/searchRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { globalSearch } = require("../../controllers/v1/searchController");
-
-const router = express.Router();
-
-router.route("/").get(globalSearch);
-
-module.exports = router;
