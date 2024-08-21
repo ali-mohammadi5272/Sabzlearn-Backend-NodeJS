cfc2b3422fc9ac5c0760f8cd4b1a92b18d2af283
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-21 07:57:17 +0330

    refactor: change userOrder router require address

--- modules/v1/userOrder/routes.js
+++ modules/v1/userOrder/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const authMiddleware = require("../../middlewares/authMiddleware");
-const { getUserOrders } = require("../../controllers/v1/userOrderController");
+const authMiddleware = require("../../../utils/middlewares/authMiddleware");
+const { getUserOrders } = require("./controller");
 
 const router = express.Router();
 
