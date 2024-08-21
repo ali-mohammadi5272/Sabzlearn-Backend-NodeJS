3c9b0b168779aa3188aab888f6ffb5fea7878662
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-21 09:06:42 +0330

    feat: create getUserOrders api

--- modules/v1/user/routes.js
+++ modules/v1/user/routes.js
@@ -10,12 +10,14 @@ const {
   freeUser,
   banUser,
   updateUser,
+  getUserOrders,
 } = require("./controller");
 
 const router = express.Router();
 
 router.use(authMiddleware);
 router.put("/", updateUser);
+router.route("/orders").get(authMiddleware, getUserOrders);
 router.put("/role/:id", accessLevelMiddleware(roles.manager), changeRole);
 router.use(accessLevelMiddleware(roles.admin));
 router.get("/", getAll);
