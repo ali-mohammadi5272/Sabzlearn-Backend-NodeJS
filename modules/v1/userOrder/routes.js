a59888c5e004de59b67fd36c3b651352fac78c17
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-21 09:10:12 +0330

    docs: remove userOrder controller and router. getUsersOrder function is now in users controller

--- modules/v1/userOrder/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authMiddleware = require("../../../utils/middlewares/authMiddleware");
-const { getUserOrders } = require("./controller");
-
-const router = express.Router();
-
-router.route("/user").get(authMiddleware, getUserOrders);
-
-module.exports = router;
