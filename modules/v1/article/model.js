0483fd6418d41a0dd575b5106f32377dfc543c89
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-26 20:01:07 +0330

    feat: create articles model

---
+++ modules/v1/article/model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 5,
+      trim: true,
+      unique: true,
+    },
+    cover: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    body: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    authorId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      trim: true,
+      required: true,
+    },
+    categoryId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+      trim: true,
+      required: true,
+    },
+    publish: {
+      type: Number,
+      enum: [0, 1],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+schema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "articleId",
+});
+
+const model = mongoose.models.Article || mongoose.model("Article", schema);
+
+module.exports = model;
