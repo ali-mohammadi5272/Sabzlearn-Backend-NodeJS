d238ec5aaac9c10c94625a8d88012e2b17a5ec70
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-27 11:59:47 +0330

    feat: create addArticle api

--- modules/v1/article/routes.js
+++ modules/v1/article/routes.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const authMiddleware = require("../../../utils/middlewares/authMiddleware");
+const accessLevelMiddleware = require("../../../utils/middlewares/accessLevelMiddleware");
+const { uploader } = require("../../../utils/uploader");
+const { roles } = require("../../../utils/constants");
+const { addArticle } = require("./controller");
 
 const router = express.Router();
+
+router
+  .route("/")
+  .post(
+    authMiddleware,
+    accessLevelMiddleware(roles.author),
+    uploader("public/articles/covers").single("upload"),
+    addArticle
+  );
 
 module.exports = router;
