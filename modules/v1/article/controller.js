3b5a8b9163f7f33d8365a94785bb81ab3df58f26
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-08-28 14:01:59 +0330

    feat: create removeArticle function

--- modules/v1/article/controller.js
+++ modules/v1/article/controller.js
@@ -85,6 +85,27 @@ const addArticle = async (req, res) => {
   }
 };
 
+const removeArticle = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+  if (!isValidId) {
+    return res.status(422).json({ message: "ArticleId is not valid !!" });
+  }
+
+  try {
+    const article = await articleModel.findOneAndDelete({ _id: id });
+    if (!article) {
+      return res.status(404).json({ message: "Article not found !!" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Article removed successfully :))" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   addArticle,
+  removeArticle,
 };
