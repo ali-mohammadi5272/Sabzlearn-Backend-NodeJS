57e0eb3ecd2a2ff9b37c79d55afb228b882ca1c5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-01 17:25:20 +0330

    feat: create updateArticle validation

---
+++ utils/validators/articles/updateArticle.js
@@ -0,0 +1,29 @@
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+const schema = {
+  type: "object",
+  properties: {
+    title: {
+      type: "string",
+    },
+    body: {
+      type: "string",
+    },
+    categoryId: {
+      type: "string",
+    },
+    href: {
+      type: "string",
+    },
+    publish: {
+      type: "number",
+    },
+  },
+  required: ["title", "body", "categoryId", "href", "publish"],
+  additionalProperties: false,
+};
+
+const validate = ajv.compile(schema);
+
+module.exports = validate;
