2d40e9a73c3d7b9666fce4ee8ef496f8b0f1fdf9
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-01 18:00:19 +0330

    feat: create getArticlesByCategory function

--- modules/v1/article/controller.js
+++ modules/v1/article/controller.js
@@ -252,10 +252,36 @@ const updateArticle = async (req, res) => {
   }
 };
 
+const getArticlesByCategory = async (req, res) => {
+  const { categoryId } = req.params;
+  const isValidId = isValidObjectId(categoryId);
+  if (!isValidId) {
+    return res.status(422).json({ message: "CategoryId is not valid !!" });
+  }
+  try {
+    const category = await categoryModel.findOne({ _id: categoryId });
+    if (!category) {
+      return res.status(404).json({ message: "Category not found !!" });
+    }
+
+    const articles = await articleModel
+      .find({ categoryId })
+      .populate("authorId", "firstname lastname")
+      .populate("categoryId", "title")
+      .select("-__v")
+      .lean();
+
+    return res.status(200).json(articles);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   addArticle,
   removeArticle,
   getAll,
   getArticle,
   updateArticle,
+  getArticlesByCategory,
 };
